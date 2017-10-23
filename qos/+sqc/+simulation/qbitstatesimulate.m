e6728c7ac0d4a53af678faaf70af0a9601476555
Ming <minggong@ustc.edu.cn>
2017-10-23 15:57:21 +0800

    fix missing file error

--- qos/+sqc/+simulation/qbitstatesimulate.m
+++ qos/+sqc/+simulation/qbitstatesimulate.m
@@ -1,9 +1,12 @@
 function result=qbitstatesimulate(celljsonorfile)
 % zhaouv https://zhaouv.github.io/
+% qutip的circuit能识别的门的列表
+% ["RX","RY","RZ","SQRTNOT","SNOT","PHASEGATE","CRX","CRY","CRZ","CPHASE","CNOT","CSIGN",
+% "BERKELEY","SWAPalpha","SWAP","ISWAP","SQRTSWAP","SQRTISWAP","FREDKIN","TOFFOLI","GLOBALPHASE"]
 
-warning('替换为 qutipenv=localconfig.pythonconfig.qutipenv;统一配置?否则若放入qos中会容易在移植时出bug')
+% warning('替换为 qutipenv=localconfig.pythonconfig.qutipenv;统一配置?否则若放入qos中会容易在移植时出bug')
 qutipenv='C:\ProgramData\Anaconda3\envs\qutip-env\python.exe';
-%[~,qutipenv,~]=pyversion;%如果默认的python环境有qutip,使用此句即可
+% [~,qutipenv,~]=pyversion;%如果默认的python环境有qutip,使用此句即可
 
 %{
 %example1
@@ -13,24 +16,12 @@ jsonstr=['[["h","cz1","rx90"],',...
     '["h","cz4","ry-90"]]'];
 result=sqc.simulation.qbitstatesimulate(jsonstr)
 
-%>>result =
+%>>result = 
 %>>
-%>>   0.2500 - 0.2500i
-%>>   0.0000 + 0.0000i
-%>>   0.2500 + 0.2500i
-%>>   0.0000 + 0.0000i
-%>>   0.0000 + 0.0000i
-%>>  -0.2500 + 0.2500i
-%>>   0.0000 + 0.0000i
-%>>   0.2500 + 0.2500i
-%>>   0.2500 - 0.2500i
-%>>   0.0000 + 0.0000i
-%>>  -0.2500 - 0.2500i
-%>>   0.0000 + 0.0000i
-%>>   0.0000 + 0.0000i
-%>>  -0.2500 + 0.2500i
-%>>   0.0000 + 0.0000i
-%>>  -0.2500 - 0.2500i
+%>>  包含以下字段的 struct:
+%>>
+%>>    real: [16×1 double]
+%>>    imag: [16×1 double]
 %%
 
 %example2
@@ -48,64 +39,33 @@ result
 %>>
 %>>  12×1 cell 数组
 %>>
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
-%>>    [4×1 double]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
+%>>    [1×1 struct]
 
-%example3
-%{
->>>longstr
-[[
-"rz90","h"   ,"cz1" ,"h"   ,"rz90","h"   ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,"cz1" ,"h"   ,""    ,""    
-],[
-"h"   ,"cz1" ,"cz2" ,"rz90","h"   ,""    ,""    ,""    ,""    ,""    ,""    ,"cz1" ,"h"   ,"cz1" ,"h"   ,"cz1" ,"h"   ,"cz2" ,"h"   ,"cz1" ,""    
-],[
-"h"   ,"cz2" ,"cz3" ,"rz90",""    ,""    ,"cz1" ,"h"   ,"cz1" ,"h"   ,"cz1" ,"cz2" ,"h"   ,"cz2" ,"h"   ,"cz2" ,"h"   ,""    ,""    ,"cz2" ,"h"   
-],[
-"h"   ,"cz3" ,"cz4" ,"h"   ,"rz-90","h"   ,"cz2" ,"h"   ,"cz2" ,"h"   ,"cz2" ,"h"   ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    
-],[
-"h"   ,"cz4" ,"rz90",""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    ,""    
-]]
-%}
-jsonstr=qes.util.stringlines('longstr',mfilename('fullpath'));
-result=sqc.simulation.qbitstatesimulate(jsonstr)
-
-%example4
-result=sqc.simulation.qbitstatesimulate()
-%input no args and a ui will show to choose file
-
-%example5
-result=sqc.simulation.qbitstatesimulate({'file','c:\abc\e.csv'})
-
+%目前只支持cell和json不支持文件
 %}
 
 if ~nargin
-    %celljsonorfile='[["","","ry90","cz1","rx-90","ry-90","","rx-90","cz1","","ry90","rx90"],["rx-90","ry90","rx90","cz2","","ry90","x","ry-90","cz2","rx-90","ry-90","rx90"]]';
-    [namestr,pathstr]=uigetfile({'*.csv';'*.txt';'*.in'});
-    arcstr=['"',pathstr,namestr,'"'];
-    stringorfile=['file ',arcstr];
-elseif iscell(celljsonorfile)
-    if isstr(celljsonorfile{1}) && strcmp(celljsonorfile{1},'file')
-        stringorfile=['file "',celljsonorfile{2},'"'];
-    else
-        stringorfile=tempcell2str(celljsonorfile);
-    end
+    celljsonorfile='[["","","ry90","cz1","rx-90","ry-90","","rx-90","cz1","","ry90","rx90"],["rx-90","ry90","rx90","cz2","","ry90","x","ry-90","cz2","rx-90","ry-90","rx90"]]';
+end
+
+if iscell(celljsonorfile)
+    stringorfile=tempcell2str(celljsonorfile);
 else
     celljsonorfile=jsondecode(celljsonorfile);    
     stringorfile=tempcell2str(celljsonorfile);
 end
 %stringorfile=',,ry90,cz1,rx-90,ry-90,,rx-90,cz1,,ry90,rx90#rx-90,ry90,rx90,cz2,,ry90,x,ry-90,cz2,rx-90,ry-90,rx90';
-
-
 
 mpath = mfilename('fullpath');
 i=findstr(mpath,'\'); %#ok<FSTR>
@@ -118,7 +78,7 @@ if ~(status==0)
 end
 
 result=jsondecode(result);
-
+result=-result.real-1j*result.imag;
 end
 
 function str=tempcell2str(tempcell)
